c2268b372ad6dde2565c8881eea202bb22380fe8
Kesarwani-123 <kajalkesharwani171@gmail.com>
2024-08-07 17:02:16 +0530

    Added code to retrieve user list via api

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        {/* Add other routes here */}
+        
       </Routes>
     </Router>
   );
